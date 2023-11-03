e8c0ff21ea33e262479a1fc4232f065a09058dc5
IanoNjuguna <inc3099@gmail.com>
2023-11-03 23:31:19 +0300

    incorporate 'Datta Able' theme

--- IMS/settings.py
+++ IMS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_datta.apps.AdminDattaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ZenVentory
 
 An Inventory Management System
+
+## Admin Dashboard
+
+1. THEME: [django-admin-datta](https://pypi.org/project/django-admin-datta/)
